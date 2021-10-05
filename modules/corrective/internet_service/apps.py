638b30cf491790551e3fb96acab8fdac6cd319ff
ictsu <ictsu@buksu.edu.ph>
2021-10-05 09:57:00 +0800

    Add empty app for internet services srf module

---
+++ modules/corrective/internet_service/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class InternetServiceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'modules.corrective.internet_service'
+
